6fcc2b35068b2a79578c545e1d95d1214fbd92c7
Christofer Bryan <christoferbryan45@gmail.com>
2024-07-12 13:56:28 +0700

    feat: added point entity to store points history of users

---
+++ src/main/java/com/events/eventmanagement/point/entity/Point.java
@@ -0,0 +1,49 @@
+package com.events.eventmanagement.point.entity;
+
+import com.events.eventmanagement.users.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Data
+@Entity
+@Table(name = "point", schema = "public")
+public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_gen")
+    @SequenceGenerator(name = "users_id_gen", sequenceName = "users_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "points", nullable = false)
+    private int points;
+
+    @NotNull
+    @ManyToOne
+    @Column(name = "user_id", nullable = false)
+    private User user;
+
+    @NotNull
+    @Column(name = "expired_at", nullable = false)
+    private Instant expiredAt;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+}
