676471d6d2220dcffd019824e68d6f200c31da22
Christofer Bryan <christoferbryan45@gmail.com>
2024-07-14 21:55:50 +0700

    feat: added event and category entities

---
+++ src/main/java/com/events/eventmanagement/category/entity/Category.java
@@ -0,0 +1,29 @@
+package com.events.eventmanagement.category.entity;
+
+import com.events.eventmanagement.event.entity.Event;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "category", schema = "public")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private Set<Event> events = new LinkedHashSet<>();
+}
---
+++ src/main/java/com/events/eventmanagement/event/entity/Event.java
@@ -0,0 +1,81 @@
+package com.events.eventmanagement.event.entity;
+
+import com.events.eventmanagement.category.entity.Category;
+import com.events.eventmanagement.users.entity.User;
+import com.events.eventmanagement.coupon.entity.Coupon;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "event", schema = "public")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Size(max = 250)
+    @NotNull
+    @Column(name = "description", nullable = false, length = 250)
+    private String description;
+
+    @Column(name = "picture")
+    private String picture;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "location", nullable = false, length = 100)
+    private String location;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @NotNull
+    @Column(name = "time", nullable = false)
+    private Instant time;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "organizer_id", nullable = false)
+    private User organizer;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private Set<Coupon> coupons = new LinkedHashSet<>();
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+}
