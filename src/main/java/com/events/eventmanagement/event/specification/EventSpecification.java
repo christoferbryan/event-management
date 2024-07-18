234999036ca04375a8c6ec1f9308f006410056eb
Christofer Bryan <christoferbryan45@gmail.com>
2024-07-19 00:25:40 +0700

    feat: added event specification

---
+++ src/main/java/com/events/eventmanagement/event/specification/EventSpecification.java
@@ -0,0 +1,47 @@
+package com.events.eventmanagement.event.specification;
+
+import com.events.eventmanagement.event.entity.Event;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+
+public class EventSpecification {
+    public static Specification<Event> byTitle(String title) {
+        return ((root, query, builder) -> {
+            if (title == null) {
+                return builder.conjunction();
+            }
+            if (title.isEmpty()) {
+                return builder.equal(root.get("title"), "");
+            }
+            return builder.like(builder.lower(root.get("title")), "%" + title.toLowerCase() + "%");
+        });
+    }
+
+    public static Specification<Event> byCategory(String category) {
+        return ((root, query, builder) -> {
+            if (category == null) {
+                return builder.conjunction();
+            }
+            return builder.equal(builder.lower(root.get("category")), category.toLowerCase());
+        });
+    }
+
+    public static Specification<Event> byUserId(Long userId) {
+        return ((root, query, builder)-> {
+            if (userId == null) {
+                return builder.conjunction();
+            }
+            return builder.equal(root.get("organizer").get("id"), userId);
+        });
+    }
+
+    public static Specification<Event> byDate(LocalDate date) {
+        return ((root, query, builder)-> {
+            if (date == null) {
+                return builder.conjunction();
+            }
+            return builder.equal(root.get("date"), date);
+        });
+    }
+}
